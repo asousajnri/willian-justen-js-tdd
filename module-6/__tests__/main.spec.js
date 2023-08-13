d09401a40c2971f0e48e9fbe99e51f6485e0f0ca
Antônio <antonio.dev@hublocal.com.br>
2023-08-13 10:00:22 -0300

    test struct exemple

---
+++ module-6/__tests__/main.spec.js
@@ -0,0 +1,17 @@
+describe('Main', () => {
+    describe('Method A', () => {
+        context('Case 1', () => {
+            it('should happen blabla', () => {
+                // espera que aconteça
+                // Entra de dados / método sum(1, 2)
+                // Espera retornar (4) => true | false => broken test
+            })
+        });
+        context('Case 2', () => {
+
+        });
+    });
+    describe('Method A', () => {
+
+    });
+});
