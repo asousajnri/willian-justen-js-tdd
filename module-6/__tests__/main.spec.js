305dc6ac0893fe255b3aec4b12b8bbd883e2b045
Antônio <antonio.dev@hublocal.com.br>
2023-08-13 10:10:11 -0300

    mocha reporters, bail, skip and only features

--- module-6/__tests__/main.spec.js
+++ module-6/__tests__/main.spec.js
@@ -1,14 +1,19 @@
 describe('Main', () => {
     describe('Method A', () => {
         context('Case 1', () => {
-            it('should happen blabla', () => {
+            it.skip('should happen blabla', () => {
                 // espera que aconteça
                 // Entra de dados / método sum(1, 2)
                 // Espera retornar (4) => true | false => broken test
             })
         });
-        context('Case 2', () => {
-
+        context.only('Case 2', () => {
+            it('should happen blabla', () => {
+                throw new Error('just an error');
+            })
+            it('should happen blabla', () => {
+                throw new Error('just an error');
+            })
         });
     });
     describe('Method A', () => {
