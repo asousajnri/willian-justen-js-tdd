54d3f72775947110e63346a27a61aa8c580415d7
Antônio <antonio.dev@hublocal.com.br>
2023-08-13 10:21:55 -0300

    mocha hooks

--- module-6/__tests__/main.spec.js
+++ module-6/__tests__/main.spec.js
@@ -1,22 +1,24 @@
 describe('Main', () => {
-    describe('Method A', () => {
-        context('Case 1', () => {
-            it.skip('should happen blabla', () => {
-                // espera que aconteça
-                // Entra de dados / método sum(1, 2)
-                // Espera retornar (4) => true | false => broken test
-            })
-        });
-        context.only('Case 2', () => {
-            it('should happen blabla', () => {
-                throw new Error('just an error');
-            })
-            it('should happen blabla', () => {
-                throw new Error('just an error');
-            })
-        });
+    // roda uma vez, antes do bloco
+    before(() => {
+        console.log('before');
     });
-    describe('Method A', () => {
-
+    // roda uma vez, depois do bloco
+    after(() => {
+        console.log('after');
+    });
+    // roda toda vez, antes do bloco
+    beforeEach(() => {
+        console.log('beforeEach');
+    });
+    // roda toda vez, depois do bloco
+    afterEach(() => {
+        console.log('afterEach');
+    });
+    it('test 1', () => {
+        console.log('test 1');
+    });
+    it('test 2', () => {
+        console.log('test 2');
     });
 });
