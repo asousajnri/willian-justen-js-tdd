21bec376d258ab758fcd0e46e64f95c316c66a2c
Antônio <antonio.dev@hublocal.com.br>
2023-08-13 12:11:08 -0300

    assertions with chai with expect

--- module-6/__tests__/main.spec.js
+++ module-6/__tests__/main.spec.js
@@ -1,24 +1,39 @@
+const { expect } = require('chai');
+
 describe('Main', () => {
+    let arr;
     // roda uma vez, antes do bloco
     before(() => {
-        console.log('before');
+
     });
     // roda uma vez, depois do bloco
     after(() => {
-        console.log('after');
+
     });
     // roda toda vez, antes do bloco
     beforeEach(() => {
-        console.log('beforeEach');
+        arr = [1, 2, 3];
     });
     // roda toda vez, depois do bloco
     afterEach(() => {
-        console.log('afterEach');
+
     });
-    it('test 1', () => {
-        console.log('test 1');
+    it('should be an array', () => {
+        expect(arr).to.be.an('array');
     });
-    it('test 2', () => {
-        console.log('test 2');
+    it('should have a size of 4 when push another value to the array', () => {
+        arr.push(4);
+        expect(arr).to.have.lengthOf(4);
+    });
+    it('should remove the value 3 when use pop in the array', () => {
+        arr.pop()
+        expect(arr).to.not.include(3);
+    });
+    it('should return true when pop 3 from the array', () => {
+        expect(arr.pop() === 3).to.be.true;
+    });
+    it('should have a size of 2 when pop a value from the array', () => {
+        arr.pop();
+        expect(arr).to.have.lengthOf(2);
     });
 });
