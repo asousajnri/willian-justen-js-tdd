ba7595465d5743a10fffa2ad4b28c31bcd8ecce0
Antônio <a.sousa.jnri@gmail.com>
2024-02-28 23:08:26 -0300

    Lesson 100, to prepare lib

---
+++ module-7/lib/search.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.searchPlaylists = exports.searchTracks = exports.searchArtists = exports.searchAlbums = exports.search = undefined;
+
+var _config = require('./config');
+
+var search = exports.search = function search(q, t) {
+  return fetch(_config.API_URL + '/search?q=' + q + '&type=' + t).then(function (data) {
+    return data.json();
+  });
+};
+
+var searchAlbums = exports.searchAlbums = function searchAlbums(q) {
+  return search(q, 'album');
+};
+
+var searchArtists = exports.searchArtists = function searchArtists(q) {
+  return search(q, 'artist');
+};
+
+var searchTracks = exports.searchTracks = function searchTracks(q) {
+  return search(q, 'track');
+};
+
+var searchPlaylists = exports.searchPlaylists = function searchPlaylists(q) {
+  return search(q, 'playlist');
+};
