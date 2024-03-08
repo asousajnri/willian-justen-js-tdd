596575d38e771f866dbb5d57904ba29bffbe7a90
Antônio <a.sousa.jnri@gmail.com>
2024-03-08 07:53:09 -0300

    Add travis ci

--- module-7/webpack.config.babel.js
+++ module-7/webpack.config.babel.js
@@ -1,22 +1,18 @@
-import { join } from 'path';
+import {join} from 'path'
 
-const include = join(__dirname, 'src');
+const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/index',
+  entry: './index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'sporifyWrapper'
+    library: 'SpotifyWrapper',
   },
   devtool: 'source-map',
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include
-      }
+      {test: /\.js$/, loader: 'babel-loader', include},
     ]
   }
-};
+}
